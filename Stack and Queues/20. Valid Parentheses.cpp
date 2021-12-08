6060cca5de5989ca6c335661e60b822d76693384
Karanveer Singh <karanvs464@gmail.com>
2021-12-08 18:28:11 +0530

    Done

---
+++ Stack and Queues/20. Valid Parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        
+        for(auto it : s)
+        {
+            if(it == '(' || it == '{' || it == '[')
+                st.push(it);
+            
+            else
+            {
+                if(st.empty())
+                    return false;
+                
+                char ch = st.top();
+                st.pop();
+                
+                if( (it == ')' && ch != '(' ) || (it == '}' && ch != '{' ) || (it == ']' && ch != '[') )
+                    return false;                                       
+            }
+        }
+        
+        return st.empty();
+    }
+};
+
+// https://leetcode.com/problems/valid-parentheses/
