3d2088163accca1ee72ac04a14c9e407ac4c7d51
Karanveer Singh <karanvs464@gmail.com>
2021-12-25 20:51:54 +0530

    Done

---
+++ Heaps/23. Merge k Sorted Lists.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Compare
+{
+public:
+    bool operator()(ListNode *A, ListNode *B)
+    {
+        return A->val > B->val;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& A) 
+    {
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+        ListNode *dummy = new ListNode(-1);
+        ListNode *tail = dummy;
+
+        for (int i = 0; i < A.size(); i++)
+            if (A[i] != NULL)
+                pq.push(A[i]);
+
+        while (!pq.empty())
+        {
+            ListNode *temp = pq.top();
+            pq.pop();
+
+            tail->next = temp;
+            tail = temp;
+
+            if (temp->next != NULL)
+                pq.push(temp->next);
+        }
+
+        return dummy->next;    
+    }
+};
+
+// 
