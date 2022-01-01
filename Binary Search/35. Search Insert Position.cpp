4bc2677d1d1fe66ae06588f7e5ce4a16978eee53
Karanveer Singh <karanvs464@gmail.com>
2022-01-01 21:11:10 +0530

    Done

---
+++ Binary Search/35. Search Insert Position.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& A, int target) 
+    {
+        int start = 0;
+        int end = A.size() - 1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (A[mid] == target)
+                return mid;
+            if (target < A[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return start;    
+    }
+};
+
+// https://leetcode.com/problems/search-insert-position/submissions/
