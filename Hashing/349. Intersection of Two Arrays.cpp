cca1f4adadd71fba9aa16b3307b4ba9ad5974fb3
Karanveer Singh <karanvs464@gmail.com>
2022-01-05 13:15:19 +0530

    Done

---
+++ Hashing/349. Intersection of Two Arrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        vector<int> ans;
+        unordered_map<int, int> m;
+        
+        for(int i = 0; i < nums1.size(); i++)
+            m[nums1[i]]++;
+        
+        for(int i = 0; i < nums2.size(); i++)
+        {
+            int x = nums2[i];
+            
+            if(m.find(x) != m.end() && m[x] > 0)
+            {
+               ans.push_back(x);
+               m[x] = 0;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+// https://leetcode.com/problems/intersection-of-two-arrays/submissions/
