b9b76b4f8ed18a733625c95893066ba069f1d137
Karanveer Singh <karanvs464@gmail.com>
2022-01-17 23:21:38 +0530

    Done

---
+++ Arrays/53. Maximum Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum = 0;
+        int ans = INT_MIN;
+        
+        for(auto it : nums)
+        {
+            sum += it;
+            ans = max(ans, sum);
+            if(sum < 0)
+                sum = 0;
+        }
+        
+        return ans;
+    }
+};
+
+// https://leetcode.com/problems/maximum-subarray/
