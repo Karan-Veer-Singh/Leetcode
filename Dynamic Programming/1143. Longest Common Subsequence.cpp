746932bc7662ef16476f87446f694a00d3a82e61
Karanveer Singh <karanvs464@gmail.com>
2022-01-21 20:50:53 +0530

    Done

--- Dynamic Programming/1143. Longest Common Subsequence.cpp
+++ Dynamic Programming/1143. Longest Common Subsequence.cpp
@@ -33,3 +33,29 @@ public:
 };
 
 
+// Bottom-Up (Tabulation)
+// Time Complexity = O(n*m)
+// Sapce Complexity = O(n*m)
+int longestCommonSubsequence(string A, string B)
+{
+    int n = A.size();
+    int m = B.size();
+
+    int dp[n + 1][m + 1];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+
+            else if (A[i - 1] == B[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[n][m];
+}
