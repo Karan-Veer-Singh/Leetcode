f279acedb5e030ebb4f6f6b43451b76550f1b830
Karanveer Singh <karanvs464@gmail.com>
2022-01-29 19:53:19 +0530

    Done

---
+++ Dynamic Programming/55. Jump Game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int n = 0;
+        int reachable = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(reachable < i)
+                return false;
+            
+            reachable = max(reachable, i+nums[i]);
+        }
+        
+        return true;
+    }
+};
+
+// https://leetcode.com/problems/jump-game/
