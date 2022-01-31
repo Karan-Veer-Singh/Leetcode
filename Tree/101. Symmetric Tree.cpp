5c951b73634910f3da19d524424a10c75f85e8d2
Karanveer Singh <karanvs464@gmail.com>
2022-01-31 13:39:06 +0530

    Done

---
+++ Tree/101. Symmetric Tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) 
+    {
+        if(root == NULL)
+            return true;
+        
+        queue<TreeNode*> q;
+        
+        q.push(root -> left);
+        q.push(root -> right);
+        
+        TreeNode *leftNode, *rightNode;
+        
+        while(!q.empty())
+        {
+            leftNode = q.front();
+            q.pop();
+            
+            rightNode = q.front();
+            q.pop();
+            
+            if(leftNode == NULL && rightNode == NULL)
+                continue;
+            
+            if((leftNode == NULL && rightNode != NULL) || (rightNode == NULL && leftNode != NULL))
+                return false;
+            
+            if(leftNode -> val != rightNode -> val)
+                return false;
+              
+            
+            q.push(leftNode -> left);   q.push(rightNode -> right);
+            q.push(leftNode -> right);  q.push(rightNode -> left);
+        }
+        
+        return true;
+    }
+};
+
+// https://leetcode.com/problems/symmetric-tree/submissions/
