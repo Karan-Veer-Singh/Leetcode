ff26f28ad713c6d111603786f95c34de094f8498
Karanveer Singh <karanvs464@gmail.com>
2022-03-13 19:38:11 +0530

    Done

--- Dynamic Programming/1143. Longest Common Subsequence.cpp
+++ Dynamic Programming/1143. Longest Common Subsequence.cpp
@@ -7,28 +7,28 @@
 class Solution {
 public:
     int dp[1005][1005];
-    
+
     int LCS(string &A, string &B, int n, int m)
     {
-        if(n == 0 || m == 0)
+        if (n == 0 || m == 0)
             return dp[n][m] = 0;
-        
-        if(dp[n][m] != -1)
+
+        if (dp[n][m] != -1)
             return dp[n][m];
-        
-        if(A[n-1] == B[m-1])
-            return dp[n][m] = 1 + LCS(A, B, n-1, m-1);
-        
-            return dp[n][m] = max(LCS(A, B, n-1, m), LCS(A, B, n, m-1));
+
+        if (A[n - 1] == B[m - 1])
+            return dp[n][m] = 1 + LCS(A, B, n - 1, m - 1);
+
+        return dp[n][m] = max(LCS(A, B, n - 1, m), LCS(A, B, n, m - 1));
     }
-    
-    int longestCommonSubsequence(string A, string B) 
+
+    int longestCommonSubsequence(string A, string B)
     {
         int n = A.size();
         int m = B.size();
-        
+
         memset(dp, -1, sizeof(dp));
-        
+
         return LCS(A, B, n, m);
     }
 };
@@ -68,30 +68,62 @@ int longestCommonSubsequence(string A, string B)
 // Time Complexity = O(n*m)
 // Sapce Complexity = O(2*N)
 class Solution {
-public: 
-    int longestCommonSubsequence(string A, string B) 
+public:
+    int longestCommonSubsequence(string A, string B)
     {
         int n = A.size();
         int m = B.size();
-        
-        int dp[2][m+1];
-      
-        for(int i = 0; i <= n; i++)
+
+        int dp[2][m + 1];
+
+        for (int i = 0; i <= n; i++)
         {
-            for(int j = 0; j <= m; j++)
+            for (int j = 0; j <= m; j++)
             {
-                if(i == 0 || j == 0)
-                    dp[i%2][j] = 0;
-                
-                else if(A[i-1] == B[j-1])
-                    dp[i%2][j] = 1 + dp[(i+1)%2][j-1];
-                
+                if (i == 0 || j == 0)
+                    dp[i % 2][j] = 0;
+
+                else if (A[i - 1] == B[j - 1])
+                    dp[i % 2][j] = 1 + dp[(i + 1) % 2][j - 1];
+
                 else
-                    dp[i%2][j] = max(dp[(i+1)%2][j], dp[i%2][j-1]);
+                    dp[i % 2][j] = max(dp[(i + 1) % 2][j], dp[i % 2][j - 1]);
             }
         }
-        
-        
-        return dp[n%2][m];
+
+
+        return dp[n % 2][m];
+    }
+};
+
+
+// Space Optimised
+// Time Complexity = O(n*m)
+// Sapce Complexity = O(2*N)
+
+class Solution {
+public:
+    int longestCommonSubsequence(string A, string B)
+    {
+        int n = A.size();
+        int m = B.size();
+
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (A[i - 1] == B[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+
+                else
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+
+            prev = curr;
+        }
+
+        return prev[m];
     }
 };
--- Dynamic Programming/516. Longest Palindromic Subsequence.cpp
+++ Dynamic Programming/516. Longest Palindromic Subsequence.cpp
@@ -1,32 +1,62 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string a) 
-    { 
+    int longestPalindromeSubseq(string a)
+    {
         string b = a;
         reverse(b.begin(), b.end());
-        
+
         int n = a.size();
         int m = b.size();
-        
-        int dp[2][m+1];
-        
-        for(int i = 0; i <= n; i++)
+
+        int dp[2][m + 1];
+
+        for (int i = 0; i <= n; i++)
         {
-            for(int j = 0; j <= m; j++)
+            for (int j = 0; j <= m; j++)
             {
-                if(i == 0 || j == 0)
-                    dp[i%2][j] = 0;
-                
-                else if(a[i-1] == b[j-1])
-                    dp[i%2][j] = 1 + dp[(i-1)%2][j-1];
-                
+                if (i == 0 || j == 0)
+                    dp[i % 2][j] = 0;
+
+                else if (a[i - 1] == b[j - 1])
+                    dp[i % 2][j] = 1 + dp[(i - 1) % 2][j - 1];
+
                 else
-                    dp[i%2][j] = max(dp[(i-1)%2][j], dp[i%2][j-1]);
+                    dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
             }
         }
-        
-        return dp[n%2][m];
+
+        return dp[n % 2][m];
     }
 };
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string A)
+    {
+        string B = A;
+        reverse(B.begin(), B.end());
+
+        int m = A.size();
+
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (A[i - 1] == B[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+
+                else
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+};
+
 
 // https://leetcode.com/problems/longest-palindromic-subsequence/submissions/
