6df5a0ebd9017ecc19295699f6951dd747fb43f5
Karanveer Singh <karanvs464@gmail.com>
2022-03-13 21:19:23 +0530

    Done

--- Dynamic Programming/72. Edit Distance.cpp
+++ Dynamic Programming/72. Edit Distance.cpp
@@ -1,40 +1,80 @@
+// Memoization
+
 class Solution {
 public:
-    int minDistance(string A, string B) 
+    int solve(int i, int j, string &a, string &b, vector<vector<int>> &dp)
+    {
+        if (i == 0)  return j;
+        if (j == 0)  return i;
+
+        if (dp[i][j] != -1)  return dp[i][j];
+
+        if (a[i - 1] == b[j - 1])
+        {
+            return dp[i][j] = solve(i - 1, j - 1, a, b, dp);
+        }
+
+        else
+        {
+            int insert = solve(i, j - 1, a, b, dp);
+            int del = solve(i - 1, j, a, b, dp);
+            int update = solve(i - 1, j - 1, a, b, dp);
+
+            return dp[i][j] = 1 + min(insert, min(del, update));
+        }
+    }
+
+    int minDistance(string word1, string word2)
+    {
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+
+    }
+};
+
+
+// Tabulation
+
+class Solution {
+public:
+    int minDistance(string A, string B)
     {
         int n = A.size();
         int m = B.size();
 
-        int dp[2][m+1];
+        int dp[2][m + 1];
 
-        for(int i = 0; i <= n; i++)
+        for (int i = 0; i <= n; i++)
         {
-            for(int j = 0; j <= m; j++)
+            for (int j = 0; j <= m; j++)
             {
-                if(i == 0)
-                    dp[i%2][j] = j;
+                if (i == 0)
+                    dp[i % 2][j] = j;
 
-                else if(j == 0)
-                    dp[i%2][j] = i;
+                else if (j == 0)
+                    dp[i % 2][j] = i;
 
                 else
                 {
-                    if(A[i-1] == B[j-1])
-                        dp[i%2][j] = dp[(i-1)%2][j-1];
+                    if (A[i - 1] == B[j - 1])
+                        dp[i % 2][j] = dp[(i - 1) % 2][j - 1];
 
                     else
                     {
-                        int replace = dp[(i-1)%2][j-1];
-                        int del = dp[(i-1)%2][j];
-                        int insert = dp[i%2][j-1];
+                        int replace = dp[(i - 1) % 2][j - 1];
+                        int del = dp[(i - 1) % 2][j];
+                        int insert = dp[i % 2][j - 1];
 
-                        dp[i%2][j] = 1 + min({replace, del, insert});
+                        dp[i % 2][j] = 1 + min({replace, del, insert});
                     }
                 }
             }
         }
 
-        return dp[n%2][m];
+        return dp[n % 2][m];
     }
 };
 
