c58742911d23f695de506bcbe9e61ace634b462e
Karanveer Singh <karanvs464@gmail.com>
2022-03-16 19:55:01 +0530

    Done

---
+++ Graph/1020. Number of Enclaves.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &grid, int i, int j)
+    {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0)
+            return;
+
+        grid[i][j] = 0;
+
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    int numEnclaves(vector<vector<int>>& grid)
+    {
+        int row = grid.size();
+        int col = grid[0].size();
+
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                if (i == 0 || i == row - 1 || j == 0 || j == col - 1)
+                    if (grid[i][j] == 1)
+                        dfs(grid, i, j);
+
+        int count = 0;
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                if (grid[i][j] == 1)
+                    count++;
+
+        return count;
+    }
+};
+
+// https://leetcode.com/problems/number-of-enclaves/submissions/
