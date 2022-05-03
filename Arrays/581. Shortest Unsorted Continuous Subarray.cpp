5dfd7667dc9fa90c1000a487e93b8138e0a1b64c
Karanveer Singh <karanvs464@gmail.com>
2022-05-03 12:17:03 +0530

    Done

--- Arrays/581. Shortest Unsorted Continuous Subarray.cpp
+++ Arrays/581. Shortest Unsorted Continuous Subarray.cpp
@@ -1,34 +1,59 @@
 class Solution {
 public:
-    int findUnsortedSubarray(vector<int>& A) 
+    int findUnsortedSubarray(vector<int>& A)
     {
-        int left= INT_MAX;
+        int n = A.size();
+        int end = -1, max = A[0];
+        for (int i = 1; i < n; i++)
+            if (A[i] < max)  end = i;
+            else    max = A[i];
+
+        int start = 0, min = A[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+            if (A[i] > min)  start = i;
+            else min = A[i];
+
+        return end - start + 1;
+    }
+};
+
+// TC - O(N)
+// SC - O(1)
+---------------------------------------------------------------------------- -
+
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& A)
+    {
+        int left = INT_MAX;
         int right = INT_MIN;
         int n = A.size();
 
-        for(int i = 1; i < n; i++)
-            if(A[i] < A[i-1])
+        for (int i = 1; i < n; i++)
+            if (A[i] < A[i - 1])
                 left = min(left, A[i]);
 
-        for(int i = n-2; i >= 0; i--)
-            if(A[i] > A[i+1])
+        for (int i = n - 2; i >= 0; i--)
+            if (A[i] > A[i + 1])
                 right = max(right, A[i]);
 
-        if(left == INT_MAX && right == INT_MIN)
+        if (left == INT_MAX && right == INT_MIN)
             return 0;
 
-        int start = 0, end = n-1;
+        int start = 0, end = n - 1;
 
-        for(; start < n; start++)
-            if(A[start] > left)
+        for (; start < n; start++)
+            if (A[start] > left)
                 break;
 
-        for(; end >= 0; end--)
-            if(A[end] < right)
+        for (; end >= 0; end--)
+            if (A[end] < right)
                 break;
 
-        return end-start+1;   
+        return end - start + 1;
     }
 };
+
+// TC - O(N)
 
 // https://leetcode.com/problems/shortest-unsorted-continuous-subarray/submissions/
