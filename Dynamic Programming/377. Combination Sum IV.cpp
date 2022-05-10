dddef74c3813b9a5daa69d1d7f671a0b5ec502ec
Karanveer Singh <karanvs464@gmail.com>
2022-05-10 11:54:23 +0530

    Done

---
+++ Dynamic Programming/377. Combination Sum IV.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    int solve(vector<int> &nums, int target, vector<int> &dp) {
+        if (dp[target] != -1)    return dp[target];
+        int res = 0;
+        for (int i : nums)
+            if (i <= target)
+                res += solve(nums, target - i, dp);
+
+        return dp[target] = res;
+    }
+
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, -1);
+        dp[0] = 1;
+        return solve(nums, target, dp);
+    }
+};
+
+// https://leetcode.com/problems/combination-sum-iv/
