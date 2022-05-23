9f5d3a4eec7e16eacda9b81d40528d13e913d934
Karanveer Singh <karanvs464@gmail.com>
2022-05-23 21:03:52 +0530

    Done

---
+++ Stack and Queues/496. Next Greater Element I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        stack<int> s;
+        unordered_map<int, int> m;
+
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!s.empty() && s.top() < nums2[i]) s.pop();
+            int nge = s.empty() ? -1 : s.top();
+            m[nums2[i]] = nge;
+            s.push(nums2[i]);
+        }
+
+        for (int q : nums1)
+            ans.push_back(m[q]);
+
+        return ans;
+    }
+};
+
+// https://leetcode.com/problems/next-greater-element-i/
