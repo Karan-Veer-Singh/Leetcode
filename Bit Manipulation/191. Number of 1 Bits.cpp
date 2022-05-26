4d555715584ec6555fec81a64fa9fb9aa5c2a43c
Karanveer Singh <karanvs464@gmail.com>
2022-05-26 17:37:44 +0530

    Done

--- Bit Manipulation/191. Number of 1 Bits.cpp
+++ Bit Manipulation/191. Number of 1 Bits.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) 
+    int hammingWeight(uint32_t n)
     {
         int ans = 0;
-        while(n > 0)
+        while (n > 0)
         {
             ans++;
-            n = n & (n-1);
+            n = n & (n - 1);
         }
-        
+
         return ans;
     }
 };
 
-// https://leetcode.com/problems/number-of-1-bits/submissions/
+// https://leetcode.com/problems/number-of-1-bits/
