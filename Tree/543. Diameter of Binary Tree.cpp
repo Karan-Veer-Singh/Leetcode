f4e9d48b263cf39619ef8b4ea214b3185a99daa5
Karanveer Singh <karanvs464@gmail.com>
2022-05-30 11:51:42 +0530

    Done

--- Tree/543. Diameter of Binary Tree.cpp
+++ Tree/543. Diameter of Binary Tree.cpp
@@ -10,35 +10,53 @@
  * };
  */
 class Solution {
+private:
+    int height(TreeNode *root, int &diameter) {
+        if (root == NULL)    return 0;
+        int lh = height(root->left, diameter);
+        int rh = height(root->right, diameter);
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh, rh);
+    }
 public:
-    
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+};
+
+-------------------------------------------------------------------------- -
+class Solution {
+public:
+
     class Pair
     {
-        public:
-            int height;
-            int diameter;
+    public:
+        int height;
+        int diameter;
     };
-    
+
     Pair diameter(TreeNode *root)
     {
-         Pair p;
-        
-        if(root == NULL)
+        Pair p;
+
+        if (root == NULL)
         {
             p.height = p.diameter = 0;
             return p;
         }
-        
+
         Pair left = diameter(root -> left);
         Pair right = diameter(root -> right);
-        
+
         p.height = 1 + max(left.height, right.height);
         p.diameter = max({(left.height + right.height), left.diameter, right.diameter});
-        
+
         return p;
     }
-    
-    int diameterOfBinaryTree(TreeNode* root) 
+
+    int diameterOfBinaryTree(TreeNode* root)
     {
         Pair p = diameter(root);
         return p.diameter;
