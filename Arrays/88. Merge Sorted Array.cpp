aacdc00a5b5ec2d697f96c8ff7fecb21d0604772
Karanveer Singh <karanvs464@gmail.com>
2022-06-07 10:21:05 +0530

    Done

---
+++ Arrays/88. Merge Sorted Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+    {
+        int pa = m - 1;
+        int pb = n - 1;
+        int pc = m + n - 1;
+
+        while (pb >= 0)
+        {
+            if (pa >= 0 && nums1[pa] > nums2[pb])
+                nums1[pc--] = nums1[pa--];
+            else
+                nums1[pc--] = nums2[pb--];
+        }
+    }
+};
+
+// https://leetcode.com/problems/merge-sorted-array/
