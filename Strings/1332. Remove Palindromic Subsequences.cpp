23a55e692ae13f2aa5e874e530ca466a67b218f0
Karanveer Singh <karanvs464@gmail.com>
2022-06-08 21:12:57 +0530

    Done

---
+++ Strings/1332. Remove Palindromic Subsequences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            //If string is not palindrome return 2
+            if (s[i] != s[j]) return 2;
+            i++;
+            j--;
+        }
+
+        return 1;
+
+    }
+};
+
+// https://leetcode.com/problems/remove-palindromic-subsequences/
