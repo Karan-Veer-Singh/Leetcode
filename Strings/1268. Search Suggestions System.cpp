fb35d8b1621630b708eecc8d1956ad60ba313d96
Karanveer Singh <karanvs464@gmail.com>
2022-06-19 11:02:28 +0530

    Done

---
+++ Strings/1268. Search Suggestions System.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(), products.end());
+        int l = 0, r = products.size() - 1;
+        vector<vector<string>> ans;
+
+        for (int i = 0; i < searchWord.size(); i++) {
+            char ch = searchWord[i];
+            vector<string> temp;
+
+            while (l <= r && products[l][i] != ch)   l++;
+            while (l <= r && products[r][i] != ch)   r--;
+            for (int j = l; j < l + min(3, r - l + 1); j++) {
+                temp.push_back(products[j]);
+            }
+
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
+
+// https://leetcode.com/problems/search-suggestions-system/
