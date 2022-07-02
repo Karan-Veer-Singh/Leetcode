4eec18031aacc503426780e2be8e31caa200a034
Karanveer Singh <karanvs464@gmail.com>
2022-07-02 12:19:37 +0530

    Done

---
+++ Arrays/1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        sort(horizontalCuts.begin(), horizontalCuts.end());
+        sort(verticalCuts.begin(), verticalCuts.end());
+        int hl = horizontalCuts.size(); int vl = verticalCuts.size();
+
+        int maxHeight = max(horizontalCuts[0], h - horizontalCuts[hl - 1]);
+        for (int i = 1; i < hl; i++) {
+            int horizontalHeight = horizontalCuts[i] - horizontalCuts[i - 1];
+            maxHeight = max(maxHeight, horizontalHeight);
+        }
+
+        int maxWidth = max(verticalCuts[0], w - verticalCuts[vl - 1]);
+        for (int i = 1; i < vl; i++) {
+            int verticalHeight = verticalCuts[i] - verticalCuts[i - 1];
+            maxWidth = max(maxWidth, verticalHeight);
+        }
+
+        long long ans = 1L * maxHeight * maxWidth;
+        return (int)(ans % 1000000007);
+    }
+};
