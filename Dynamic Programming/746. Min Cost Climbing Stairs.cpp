1b511effc2d1bbbc759b73ad4c1b5ae409d6b88f
Karanveer Singh <karanvs464@gmail.com>
2022-07-10 13:00:28 +0530

    Done

--- Dynamic Programming/746. Min Cost Climbing Stairs.cpp
+++ Dynamic Programming/746. Min Cost Climbing Stairs.cpp
@@ -1,3 +1,26 @@
+// Memoization
+
+class Solution {
+private:
+    int solve(vector<int> &cost, int n, vector<int> &dp) {
+        if (n == 0)  return cost[0];
+        if (n == 1)  return cost[1];
+
+        if (dp[n] != -1) return dp[n];
+
+        return dp[n] = cost[n] + min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
+    }
+
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1, -1);
+        return min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
+    }
+};
+
+_________________________________________________________________________
+
 // Tabulation
 
 class Solution {
@@ -47,3 +70,6 @@ public:
 
 // TC - O(N)
 // SC - O(1)
+
+
+// https://leetcode.com/problems/min-cost-climbing-stairs/
