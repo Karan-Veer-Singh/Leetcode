7753b8fe8cae9ff65faba008b065f6cce1358b49
Karanveer Singh <karanvs464@gmail.com>
2022-07-15 14:49:18 +0530

    Done

--- Graph/695. Max Area of Island.cpp
+++ Graph/695. Max Area of Island.cpp
@@ -1,11 +1,12 @@
 class Solution {
-public:
+private:
     int area(vector<vector<int>>& grid , int i, int j) {
         if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0 ) return 0;
         grid[i][j] = 0;
         return 1 + area(grid, i + 1, j) + area(grid, i - 1, j) + area(grid, i, j + 1) + area(grid,  i, j - 1);
     }
 
+public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size(), ans = 0;
 
