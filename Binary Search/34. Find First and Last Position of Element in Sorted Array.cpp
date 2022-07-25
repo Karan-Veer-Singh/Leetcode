c19223e049c22a293ee7db4f6d9489b658aef6b5
Karanveer Singh <karanvs464@gmail.com>
2022-07-25 18:53:49 +0530

    Done

--- Binary Search/34. Find First and Last Position of Element in Sorted Array.cpp
+++ Binary Search/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,56 +1,30 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& A, int target) 
+    vector<int> searchRange(vector<int>& A, int target)
     {
-        int first = -1;
-        int second = -1;
+        int first = -1, second = -1;
+        int start = 0, end = A.size() - 1;
 
-        int start = 0;
-        int end = A.size() - 1;
-
-        while (start <= end)
-        {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (A[mid] == target)
-            {
-                first = mid;
-                end = mid - 1;
-            }
-            else if (target < A[mid])
-            {
-                end = mid - 1;
-            }
-            else
-            {
-                start = mid + 1;
-            }
+            if (A[mid] == target) { first = mid; end = mid - 1; }
+            else if (target < A[mid])   end = mid - 1;
+            else    start = mid + 1;
         }
 
-        start = 0;
-        end = A.size() - 1;
+        start = 0, end = A.size() - 1;
 
-        while (start <= end)
-        {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (A[mid] == target)
-            {
-                second = mid;
-                start = mid + 1;;
-            }
-            else if (target < A[mid])
-            {
-                end = mid - 1;
-            }
-            else
-            {
-                start = mid + 1;
-            }
+            if (A[mid] == target) { second = mid; start = mid + 1;}
+            else if (target < A[mid])    end = mid - 1;
+            else start = mid + 1;
         }
 
         return {first, second};
     }
 };
 
-// 
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
