3d21b9bc29c69b5d40436faf8d81af52656b89eb
Karanveer Singh <karanvs464@gmail.com>
2022-08-31 12:07:14 +0530

    Done

--- Graph/417. Pacific Atlantic Water Flow.cpp
+++ Graph/417. Pacific Atlantic Water Flow.cpp
@@ -3,12 +3,12 @@ public:
 
     void dfs(vector<vector<int>> &matrix, int i, int j, int prev, vector<vector<int>> &ocean)
     {
-        if(i < 0 || i >= ocean.size() || j < 0 || j >= ocean[0].size())
+        if (i < 0 || i >= ocean.size() || j < 0 || j >= ocean[0].size())
             return;
-        
-        if(matrix[i][j] < prev || ocean[i][j])
+
+        if (matrix[i][j] < prev || ocean[i][j])
             return;
-        
+
         ocean[i][j] = 1;
 
         dfs(matrix, i + 1, j, matrix[i][j], ocean);
@@ -17,33 +17,33 @@ public:
         dfs(matrix, i, j - 1, matrix[i][j], ocean);
     }
 
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix) 
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix)
     {
         vector<vector<int>> ans;
-        if(matrix.size() < 1)
+        if (matrix.size() < 1)
             return ans;
-        
+
         int row = matrix.size();
         int col = matrix[0].size();
 
         vector<vector<int>> pacific(row, vector<int>(col, 0));
         vector<vector<int>> atlantic(row, vector<int>(col, 0));
 
-        for(int i = 0; i < col; i++)
+        for (int i = 0; i < col; i++)
         {
             dfs(matrix, 0, i, INT_MIN, pacific);
-            dfs(matrix, row-1, i, INT_MIN, atlantic);
+            dfs(matrix, row - 1, i, INT_MIN, atlantic);
         }
 
-        for(int i = 0; i < row; i++)
+        for (int i = 0; i < row; i++)
         {
             dfs(matrix, i, 0, INT_MIN, pacific);
-            dfs(matrix, i, col-1, INT_MIN, atlantic);
+            dfs(matrix, i, col - 1, INT_MIN, atlantic);
         }
 
-        for(int i = 0; i < row; i++)
-            for(int j = 0; j < col; j++)
-                if(atlantic[i][j] && pacific[i][j])
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                if (atlantic[i][j] && pacific[i][j])
                 {
                     vector<int> v(2);
                     v[0] = i;
@@ -55,4 +55,4 @@ public:
     }
 };
 
-// https://leetcode.com/problems/pacific-atlantic-water-flow/submissions/
+// https://leetcode.com/problems/pacific-atlantic-water-flow/
