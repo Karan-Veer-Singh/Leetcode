1704d7e17ed9fa74aaf69e3c561b460d538d1e16
Karanveer Singh <karanvs464@gmail.com>
2022-11-05 11:12:36 +0530

    Done

--- Tries/212. Word Search II.cpp
+++ Tries/212. Word Search II.cpp
@@ -1,96 +1,67 @@
-class Node {
-private:
-    unordered_map<char, Node*> children;
-    bool EndOfWord;
-
-public:
-    Node() {
-        EndOfWord = false;
-    }
-
-    bool containsKey(char ch) {
-        return (children[ch] != NULL);
-    }
-
-    void put(char ch, Node *node) {
-        children[ch] = node;
-    }
-
-    Node *get(char ch) {
-        return children[ch];
-    }
-
-    void setEnd(bool val) {
-        EndOfWord = val;
-    }
-
-    bool isEnd() {
-        return EndOfWord;
-    }
-};
-
-
-class Trie {
-private:
-    Node *root;
-
-public:
-    Trie() {
-        root = new Node();
-    }
-
-    void insert(string word) {
-        Node *node = root;
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word[i];
-            if (!node->containsKey(ch)) {
-                node->put(ch, new Node());
-            }
-            node = node->get(ch);
-        }
-        node->setEnd(true);
-    }
-
-    void dfs(vector<vector<char>> &board, int i, int j, string temp, vector<string> &ans, Node *curr) {
-        if (curr->isEnd()) {
-            ans.push_back(temp);
-            curr->setEnd(false);
-        }
-
-        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size())    return;
-
-        char ch = board[i][j];
-        if (ch == '$' || !curr->containsKey(ch))  return;
-
-        curr = curr->get(ch);
-
-        board[i][j] = '$';
-        dfs(board, i + 1, j, temp + ch, ans, curr);
-        dfs(board, i - 1, j, temp + ch, ans, curr);
-        dfs(board, i, j + 1, temp + ch, ans, curr);
-        dfs(board, i, j - 1, temp + ch, ans, curr);
-        board[i][j] = ch;
-    }
-
-    void solve(vector<vector<char>>& board, vector<string> &ans) {
-        int r = board.size(), c = board[0].size();
-        for (int i = 0; i < r; i++)
-            for (int j = 0; j < c; j++)
-                dfs(board, i, j, "", ans, root);
-    }
-};
-
 class Solution {
 public:
+    class Node {
+    public:
+        Node* links[26] = {NULL};
+        string flag;
+        void put(char ch, Node* node) {
+            links[ch - 'a'] = node;
+        }
+        Node* next(char ch) {
+            return links[ch - 'a'];
+        }
+        void set(string w) {
+            flag = w;
+        }
+        int isEnd() {
+            return flag.length();
+        }
+    };
+    int dr[4] = {1, -1, 0, 0};
+    int dc[4] = {0, 0, -1, 1};
+    int n, m;
+    void dfs(int r, int c, vector<vector<char>>& grid, Node *trie, vector<string>&ans) {
+        if (r < 0 || r >= n || c < 0 || c >= m || grid[r][c] == '0' || trie->next(grid[r][c]) == nullptr) return;
+        char ch = grid[r][c];
+        trie = trie->next(ch);
+        if (trie->isEnd()) {
+            ans.push_back(trie->flag);
+            trie->flag = "";
+        }
+        grid[r][c] = '0';
+        for (int i = 0; i < 4; i++) {
+            dfs(r + dr[i], c + dc[i], grid, trie, ans);
+        }
+        grid[r][c] = ch;
+    }
+
+    void insert(string s, Node *root) {
+        Node *node = root;
+        for (int i = 0; i < s.length(); i++) {
+            if (node->links[s[i] - 'a'] == nullptr) {
+                node->put(s[i], new Node);
+            }
+            node = node->next(s[i]);
+        }
+        node->set(s);
+    }
+
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Trie t;
-        for (auto w : words) t.insert(w);
-
-        vector<string> ans;
-        t.solve(board, ans);
-
+        n = board.size();
+        m = board[0].size();
+        Node *trie = new Node();
+        for (auto w : words) {
+            insert(w, trie);
+        }
+        vector<string>ans;
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < m; k++) {
+                dfs(j, k, board, trie, ans);
+            }
+        }
         return ans;
     }
 };
 
 // https://leetcode.com/problems/word-search-ii/
+
