f638f5709310282ed121a06b96750d8d8bae6ecc
Karanveer Singh <karanvs464@gmail.com>
2022-11-13 15:52:39 +0530

    Done

--- Strings/151. Reverse Words in a String.cpp
+++ Strings/151. Reverse Words in a String.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    string reverseWords(string s) 
-    {
-        int i = s.length()-1;
+    string reverseWords(string s) {
+        int i = s.size() - 1;
         string ans = "";
-        
-        while(i >= 0)
-        {
-            while(i >= 0 && s[i] == ' ')  i--;
+
+        while (i >= 0) {
+            while (i >= 0 && s[i] == ' ')    i--;
             int j = i;
-            
-            if(i < 0)   break;
-            
-            while(i >= 0 && s[i] != ' ')  i--;
-            
-            string word = s.substr(i+1, j-i);
-        
-            if(ans.empty())     
-                ans += word; 
-            else                
-                ans += " " + word;
+
+            if (i < 0)   break;
+
+            while (i >= 0 && s[i] != ' ')    i--;
+
+            string temp = s.substr(i + 1, j - i);
+
+            if (ans.size() == 0)
+                ans += temp;
+            else
+                ans = ans + " " + temp;
         }
-        
+
         return ans;
     }
 };
 
-// https://leetcode.com/problems/reverse-words-in-a-string/submissions/
+// https://leetcode.com/problems/reverse-words-in-a-string/
