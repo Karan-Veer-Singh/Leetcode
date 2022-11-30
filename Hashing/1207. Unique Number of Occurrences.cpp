468c60f4608d4dfcea765f5b9657e7071ae09b2d
Karanveer Singh <karanvs464@gmail.com>
2022-11-30 12:07:52 +0530

    Done

---
+++ Hashing/1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        // Store the frequency of elements in the unordered map.
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+
+        // Store the frequency count of elements in the unordered set.
+        unordered_set<int> freqSet;
+        for (auto [key, value] : freq) {
+            freqSet.insert(value);
+        }
+
+        // If the set size is equal to the map size,
+        // It implies frequency counts are unique.
+        return freqSet.size() == freq.size();
+    }
+};
+
+// https://leetcode.com/problems/unique-number-of-occurrences/
