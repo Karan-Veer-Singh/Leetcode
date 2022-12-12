b7daccb497a3b0e390b6dc785d1dd1b890c61f8e
Karanveer Singh <karanvs464@gmail.com>
2022-12-12 20:30:30 +0530

    Done

--- Dynamic Programming/70. Climbing Stairs.cpp
+++ Dynamic Programming/70. Climbing Stairs.cpp
@@ -1,23 +1,19 @@
+
 class Solution {
 public:
-    int climbStairs(int n) 
-    {
-        if(n == 0 || n == 1)
-            return 1;
-        
-        int prev1 = 1;
+    int climbStairs(int n) {
         int prev2 = 1;
-        
-        int ans;
-        for(int i = 2; i <= n; i++)
-        {
-            ans = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = ans;
+        int prev = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        
-        return ans;
+
+        return prev;
     }
 };
 
-// https://leetcode.com/problems/climbing-stairs/submissions/
+
+// https://leetcode.com/problems/climbing-stairs/
