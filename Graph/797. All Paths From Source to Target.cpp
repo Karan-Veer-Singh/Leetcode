cd2439ae1a40c1489cb13e7482ef1fa462cf6408
Karanveer Singh <karanvs464@gmail.com>
2022-12-30 12:45:57 +0530

    Done

--- Graph/797. All Paths From Source to Target.cpp
+++ Graph/797. All Paths From Source to Target.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> graph, vector<bool> &vis, int src, int dest, 
-             vector<vector<int>> &ans, vector<int> &path)
-    {
-        if(src == dest)
-        {
+    void dfs(vector<vector<int>> graph, vector<bool> &vis, int src, int dest,
+             vector<vector<int>> &ans, vector<int> &path) {
+        if (src == dest) {
             path.push_back(dest);
             ans.push_back(path);
             path.pop_back();
             return;
         }
-        
+
         vis[src] = true;
-              
-        for(auto ngbr : graph[src])
-        {
-            if(vis[ngbr] == false)
-            {
+
+        for (auto ngbr : graph[src]) {
+            if (vis[ngbr] == false) {
                 path.push_back(src);
                 dfs(graph, vis, ngbr, dest, ans, path);
                 path.pop_back();
             }
         }
-        
+
         vis[src] = false;
     }
-    
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
-    {
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<bool> vis(n, false);
-        
+
         vector<vector<int>> ans;
         vector<int> path;
-        
-        dfs(graph, vis, 0, n-1, ans, path);
-        
+
+        dfs(graph, vis, 0, n - 1, ans, path);
+
         return ans;
     }
 };
 
 
-// https://leetcode.com/problems/all-paths-from-source-to-target/submissions/
+// https://leetcode.com/problems/all-paths-from-source-to-target/
