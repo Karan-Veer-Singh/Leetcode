2088e63154532877bd6728915755d2ac70cf8255
Karanveer Singh <karanvs464@gmail.com>
2023-01-09 19:45:26 +0530

    Done

--- Tree/144. Binary Tree Preorder Traversal.cpp
+++ Tree/144. Binary Tree Preorder Traversal.cpp
@@ -1,43 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Recursive
+------------------
+
+class Solution {
+private:
+    void recPreorder(TreeNode *root, vector<int> &preorder) {
+        if (root == NULL)    return;
+        preorder.push_back(root->val);
+        recPreorder(root->left, preorder);
+        recPreorder(root->right, preorder);
+    }
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        recPreorder(root, preorder);
+        return preorder;
+    }
+};
+
+---------------------------------------------------------------------------- -
+
+// Iterative
+----------------
+
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        
         vector<int> ans;
-        
-        if(root == NULL)
-            return ans;
-        
+
+        if (root == NULL)   return ans;
+
         stack<TreeNode*> s;
         s.push(root);
-        
-        while(!s.empty())
-        {
+
+        while (!s.empty()) {
             root = s.top();
             s.pop();
-            
+
             ans.push_back(root -> val);
-            
-            if(root -> right)
+
+            if (root -> right)
                 s.push(root -> right);
-            
-            if(root -> left)
+
+            if (root -> left)
                 s.push(root -> left);
         }
-        
-        
+
         return ans;
     }
 };
 
-// https://leetcode.com/problems/binary-tree-preorder-traversal/submissions/
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
