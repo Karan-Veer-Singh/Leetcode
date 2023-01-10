22590d4222490718f41ba92a416cee768e60b51c
Karanveer Singh <karanvs464@gmail.com>
2023-01-10 21:01:44 +0530

    Done

--- Tree/100. Same Tree.cpp
+++ Tree/100. Same Tree.cpp
@@ -1,74 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) 
-    {
-        if(p == NULL || q == NULL)
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL || q == NULL)
             return p == q;
-        
+
         return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
 
+-------------------------------------------------------------------------------------------------- -
 
 
 // Iterative
+------------ -
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isSameTree(TreeNode* root1, TreeNode* root2) 
-    {
-        if(root1 == NULL && root2 == NULL)
+    bool isSameTree(TreeNode* root1, TreeNode* root2) {
+        if (root1 == NULL && root2 == NULL)
             return true;
-        
+
         queue<TreeNode*> q;
-        
+
         q.push(root1);
         q.push(root2);
-        
+
         TreeNode *temp1, *temp2;
-        
-        while(!q.empty())
-        {
+
+        while (!q.empty()) {
             temp1 = q.front();
             q.pop();
-            
+
             temp2 = q.front();
             q.pop();
-            
-            if(temp1 == NULL && temp2 == NULL)
+
+            if (temp1 == NULL && temp2 == NULL)
                 continue;
-            
-            if(temp1 == NULL || temp2 == NULL || temp1->val != temp2->val)
+
+            if (temp1 == NULL || temp2 == NULL || temp1->val != temp2->val)
                 return false;
-            
+
             q.push(temp1 -> left);  q.push(temp2 -> left);
             q.push(temp1 -> right); q.push(temp2 -> right);
         }
-        
+
         return true;
     }
 };
 
-// https://leetcode.com/problems/same-tree/submissions/
+// https://leetcode.com/problems/same-tree/
