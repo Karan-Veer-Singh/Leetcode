287a014d9961027f732146dce6265346adb0b412
Karanveer Singh <karanvs464@gmail.com>
2023-02-08 21:16:51 +0530

    Done

--- Dynamic Programming/45. Jump Game II.cpp
+++ Dynamic Programming/45. Jump Game II.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) 
-    {
+    int jump(vector<int>& nums) {
         int jumps = 0;
         int maxReach = 0;
         int currEnd = 0;
         int n = nums.size();
-        
-        for(int i = 0; i < n-1; i++)
-        {
-            if(n <= 1)
-                return 0;
-            
-            maxReach = max(maxReach, i+nums[i]);
-            
-            if(i == currEnd)
-            {
+
+        for (int i = 0; i < n - 1; i++) {
+            if (n <= 1)  return 0;
+
+            maxReach = max(maxReach, i + nums[i]);
+
+            if (i == currEnd) {
                 jumps++;
                 currEnd = maxReach;
             }
         }
-        
+
         return jumps;
     }
 };
 
-// https://leetcode.com/problems/jump-game-ii/submissions/
+// https://leetcode.com/problems/jump-game-ii/
