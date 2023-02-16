17412217f56e9c352f674fb88fe6869d83180685
Karanveer Singh <karanvs464@gmail.com>
2023-02-16 14:10:50 +0530

    Done

--- Tree/104. Maximum Depth of Binary Tree.cpp
+++ Tree/104. Maximum Depth of Binary Tree.cpp
@@ -9,38 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        
-        int height = 0;
-        
-        if(root == NULL)
-            return height;
-        
+        if (root == NULL)    return 0;
+
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+
+        return 1 + max(lh, rh);
+    }
+};
+
+-------------------------------------------------------------------------- -
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL)    return 0;
+
         queue<TreeNode*> q;
         q.push(root);
-        
-        while(!q.empty())
-        {
-            height++;
-            
-            int n = q.size();
-            
-            for(int i = 0; i < n; i++)
-            {
-                TreeNode *f = q.front();
+
+        int level = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+
+            while (size--) {
+                TreeNode *front = q.front();
                 q.pop();
-                
-                if(f -> left)
-                    q.push(f -> left);
-                
-                if(f -> right)
-                    q.push(f -> right);
+
+                if (front->left != NULL) q.push(front->left);
+                if (front->right != NULL)    q.push(front->right);
             }
+
+            level++;
         }
-        
-        return height;
+
+        return level;
     }
 };
 
