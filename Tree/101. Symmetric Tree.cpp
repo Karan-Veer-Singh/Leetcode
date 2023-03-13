9a128e9440f8681ecb203134fa000a7945203164
Karanveer Singh <karanvs464@gmail.com>
2023-03-13 21:58:41 +0530

    Done

--- Tree/101. Symmetric Tree.cpp
+++ Tree/101. Symmetric Tree.cpp
@@ -11,38 +11,54 @@
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) 
+    bool isSymmetric(TreeNode* root)
     {
-        if(root == NULL)
+        if (root == NULL)
             return true;
-        
+
         queue<TreeNode*> q;
-        
+
         q.push(root -> left);
         q.push(root -> right);
-        
+
         TreeNode *leftNode, *rightNode;
-        
-        while(!q.empty())
+
+        while (!q.empty())
         {
             leftNode = q.front();
             q.pop();
-            
+
             rightNode = q.front();
             q.pop();
-            
-            if(leftNode == NULL && rightNode == NULL)
+
+            if (leftNode == NULL && rightNode == NULL)
                 continue;
-            
-            if(leftNode == NULL || rightNode == NULL || leftNode -> val != rightNode -> val)
+
+            if (leftNode == NULL || rightNode == NULL || leftNode -> val != rightNode -> val)
                 return false;
-            
+
             q.push(leftNode -> left);   q.push(rightNode -> right);
             q.push(leftNode -> right);  q.push(rightNode -> left);
         }
-        
+
         return true;
     }
 };
 
-// https://leetcode.com/problems/symmetric-tree/submissions/
+----------------------------------------------------------------------------------
+
+class Solution {
+private:
+    bool check(TreeNode *a, TreeNode *b) {
+        if (a == NULL || b == NULL) return a == b;
+        return (a->val == b->val) && check(a->left, b->right) && check(a->right, b->left);
+    }
+
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL)    return true;
+        return check(root->left, root->right);
+    }
+};
+
+// https://leetcode.com/problems/symmetric-tree/
