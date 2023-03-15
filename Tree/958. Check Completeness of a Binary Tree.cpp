c478886ec13b5b3d1c400482290b86bc793e5dcb
Karanveer Singh <karanvs464@gmail.com>
2023-03-15 22:57:45 +0530

    Done

---
+++ Tree/958. Check Completeness of a Binary Tree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+
+        bool nullNodeFound = false;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (node == nullptr) {
+                nullNodeFound = true;
+            } else {
+                if (nullNodeFound) {
+                    return false;
+                }
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        return true;
+    }
+};
+
+// https://leetcode.com/problems/check-completeness-of-a-binary-tree/
