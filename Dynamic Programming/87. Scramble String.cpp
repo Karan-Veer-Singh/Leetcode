5117c85ca36dac087991f683e376d1e7a54560ff
Karanveer Singh <karanvs464@gmail.com>
2023-03-30 18:19:43 +0530

    Done

--- Dynamic Programming/87. Scramble String.cpp
+++ Dynamic Programming/87. Scramble String.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     unordered_map<string, bool> m;
-    bool isScramble(string A, string B) 
+    bool isScramble(string A, string B)
     {
-        if(A.length() != B.length())
+        if (A.length() != B.length())
             return false;
-    
+
         int n = A.length();
 
-        if(n == 0)
+        if (n == 0)
             return true;
 
-        if(A == B)
+        if (A == B)
             return true;
 
         string ta = A;
@@ -20,25 +20,25 @@ public:
         sort(ta.begin(), ta.end());
         sort(tb.begin(), tb.end());
 
-        if(ta != tb)
+        if (ta != tb)
             return false;
 
         string key = A + " " + B;
 
-        if(m.find(key) != m.end())
+        if (m.find(key) != m.end())
             return m[key];
 
-        for(int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
-            if(isScramble(A.substr(0, i), B.substr(0, i)) && isScramble(A.substr(i, n-i), B.substr(i, n-i)))
+            if (isScramble(A.substr(0, i), B.substr(0, i)) && isScramble(A.substr(i, n - i), B.substr(i, n - i)))
                 return m[key] = true;
 
-            if(isScramble(A.substr(0, i), B.substr(n-i, i)) && isScramble(A.substr(i, n-i), B.substr(0, n-i)))
+            if (isScramble(A.substr(0, i), B.substr(n - i, i)) && isScramble(A.substr(i, n - i), B.substr(0, n - i)))
                 return m[key] = true;
         }
 
-        return m[key] = false;    
+        return m[key] = false;
     }
 };
 
-// https://leetcode.com/problems/scramble-string/submissions/
+// https://leetcode.com/problems/scramble-string/
