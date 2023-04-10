56e9a0a04e0e6901eef5a550f67d7077960e43d3
Karanveer Singh <karanvs464@gmail.com>
2023-04-10 11:28:28 +0530

    Done

--- Stack and Queues/20. Valid Parentheses.cpp
+++ Stack and Queues/20. Valid Parentheses.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
         stack<char> st;
-        
-        for(auto it : s)
+
+        for (auto it : s)
         {
-            if(it == '(' || it == '{' || it == '[')
+            if (it == '(' || it == '{' || it == '[')
                 st.push(it);
-            
+
             else
             {
-                if(st.empty())
+                if (st.empty())
                     return false;
-                
+
                 char ch = st.top();
                 st.pop();
-                
-                if( (it == ')' && ch != '(' ) || (it == '}' && ch != '{' ) || (it == ']' && ch != '[') )
-                    return false;                                       
+
+                if ( (it == ')' && ch != '(' ) || (it == '}' && ch != '{' ) || (it == ']' && ch != '[') )
+                    return false;
             }
         }
-        
+
         return st.empty();
     }
 };
