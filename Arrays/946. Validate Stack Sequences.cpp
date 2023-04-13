2af6b55f9ee60ad613f32cb45c18d1087c5067ff
Karanveer Singh <karanvs464@gmail.com>
2023-04-13 17:47:22 +0530

    Done

---
+++ Arrays/946. Validate Stack Sequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped)
+    {
+        int i = 0, j = 0;
+
+        for (int val : pushed)
+        {
+            pushed[i] = val;
+
+            while (i >= 0 && pushed[i] == popped[j])
+            {
+                i--;
+                j++;
+            }
+
+            i++;
+        }
+
+        return j == popped.size();
+
+    }
+};
+
+// https://leetcode.com/problems/validate-stack-sequences/
