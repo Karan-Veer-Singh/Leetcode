e0b437c043fa9abd197388526550e18033908102
Karanveer Singh <karanvs464@gmail.com>
2023-04-24 13:01:45 +0530

    Done

--- Heaps/1046. Last Stone Weight.cpp
+++ Heaps/1046. Last Stone Weight.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         priority_queue<int> maxHeap(stones.begin(), stones.end());
+
         while (maxHeap.size() >= 2) {
             int first = maxHeap.top();  maxHeap.pop();
             int second = maxHeap.top(); maxHeap.pop();
             if (first - second > 0)    maxHeap.push(first - second);
         }
+
         if (maxHeap.empty()) return 0;
         return maxHeap.top();
     }
