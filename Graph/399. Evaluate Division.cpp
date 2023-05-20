39342aa7dea97c4941e972874a695e970b485d68
Karanveer Singh <karanvs464@gmail.com>
2023-05-20 11:47:23 +0530

    Done

--- Graph/399. Evaluate Division.cpp
+++ Graph/399. Evaluate Division.cpp
@@ -3,14 +3,12 @@ private:
     vector<double> res;
     unordered_map<string, list<pair<string, double>>> adj;
 
-    void addEdge(string u, string v, double x)
-    {
+    void addEdge(string u, string v, double x) {
         adj[u].push_back(make_pair(v, x));
         adj[v].push_back(make_pair(u, 1 / x));
     }
 
-    double bfs(string src, string dest)
-    {
+    double bfs(string src, string dest) {
         if (adj.find(src) == adj.end() || adj.find(dest) == adj.end())  return -1.0;
 
         unordered_map<string, bool>visited;
@@ -19,18 +17,15 @@ private:
         q.push({src, 1});
         visited[src] = true;
 
-        while (!q.empty())
-        {
+        while (!q.empty()) {
             auto f = q.front();    q.pop();
             string node = f.first;
             double value = f.second;
 
             if (node == dest)    return value;
 
-            for (auto ngbr : adj[node])
-            {
-                if (visited[ngbr.first] == false)
-                {
+            for (auto ngbr : adj[node]) {
+                if (visited[ngbr.first] == false) {
                     q.push({ngbr.first, value * ngbr.second});
                     visited[ngbr.first] = true;
                 }
@@ -45,8 +40,7 @@ public:
             addEdge(equations[i][0], equations[i][1], values[i]);
 
         int n = queries.size();
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             string src = queries[i][0];
             string dest = queries[i][1];
             double p = bfs(src, dest);
@@ -56,4 +50,4 @@ public:
     }
 };
 
-// https://leetcode.com/problems/evaluate-division/submissions/
+// https://leetcode.com/problems/evaluate-division/
