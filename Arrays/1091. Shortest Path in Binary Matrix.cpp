1024f53aceae57de4373e9033881f33da37e05e9
Karanveer Singh <karanvs464@gmail.com>
2023-06-01 16:07:31 +0530

    Done

--- Arrays/1091. Shortest Path in Binary Matrix.cpp
+++ Arrays/1091. Shortest Path in Binary Matrix.cpp
@@ -9,11 +9,14 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
+
         if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) return -1;
         if (n == 1 && grid[0][0] == 0) return 1;
+
         queue<pair<int, pair<int, int>>> q;
         grid[0][0] = 1;
         q.push({1, {0, 0}});
+
         while (!q.empty()) {
             int dist = q.front().first;
             int r = q.front().second.first;
