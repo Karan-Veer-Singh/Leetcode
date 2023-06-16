fe249ddfed7aacb31007fdd2b14233bef977a73a
Karanveer Singh <karanvs464@gmail.com>
2023-06-16 20:08:24 +0530

    Done

--- Tree/1569. Number of Ways to Reorder Array to Get Same BST.cpp
+++ Tree/1569. Number of Ways to Reorder Array to Get Same BST.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
+    vector < vector < long long > > pascal;
+    int MOD = 1e9 + 7;
+
     int numOfWays(vector<int>& nums) {
-        int m = nums.size();
-        // Table of Pascal's triangle
-        table.resize(m + 1);
-        for (int i = 0; i < m + 1; ++i) {
-            table[i] = vector<long long>(i + 1, 1);
-            for (int j = 1; j < i; ++j) {
-                table[i][j] = (table[i - 1][j - 1] + table[i - 1][j]) % mod;
+        int N = nums.size();
+        pascal = vector < vector < long long > > (N + 1);
+        for (int i = 0; i < N + 1; i ++) {
+            pascal[i] = vector < long long > (i + 1, 1);
+            for (int j = 1; j < i; j ++) {
+                pascal[i][j] = (pascal[i - 1][j] + pascal[i - 1][j - 1]) % MOD;
             }
         }
-
-        return (dfs(nums) - 1) % mod;
+        return dfs(nums) - 1;
     }
 
-private:
-    vector<vector<long long>> table;
-    long long mod = 1e9 + 7;
-
-    long long dfs(vector<int> &nums) {
-        int m = nums.size();
-        if (m < 3) {
+    int dfs(vector < int > nums) {
+        if (nums.size() <= 2)
             return 1;
+        vector < int > left, right;
+        for (int i = 1; i < int(nums.size()); i ++) {
+            if (nums[i] < nums[0])
+                left.push_back(nums[i]);
+            else
+                right.push_back(nums[i]);
         }
-
-        vector<int> leftNodes, rightNodes;
-        for (int i = 1; i < m; ++i) {
-            if (nums[i] < nums[0]) {
-                leftNodes.push_back(nums[i]);
-            } else {
-                rightNodes.push_back(nums[i]);
-            }
-        }
-
-        long long leftWays = dfs(leftNodes) % mod;
-        long long rightWays = dfs(rightNodes) % mod;
-
-        return (((leftWays * rightWays) % mod) * table[m - 1][leftNodes.size()]) % mod;
+        return (((dfs(left) * pascal[nums.size() - 1][left.size()]) % MOD) * dfs(right)) % MOD;
     }
 };
 
